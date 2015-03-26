fedf9c76f6c3d733e29691f2310251c50145b037
Aryan TaheriMonfared <aryantaheri@gmail.com>
2015-03-26 15:40:17 +0100

    check missing inputs

--- src/main/java/no/uis/ux/cipsi/net/monitoringbalancing/domain/stats/NodeComparator.java
+++ src/main/java/no/uis/ux/cipsi/net/monitoringbalancing/domain/stats/NodeComparator.java
@@ -10,6 +10,7 @@ public class NodeComparator implements Comparator<Node>{
     }
     @Override
     public int compare(Node n1, Node n2) {
+        if (n1 == null || n2 == null || n1.getId() == null || n2.getId() == null) return 0;
         return Integer.valueOf(n1.getId().replaceAll("[^0-9]", "")).compareTo(Integer.valueOf(n2.getId().replaceAll("[^0-9]", "")));
         //        return n1.getId().compareTo(n2.getId());
     }
