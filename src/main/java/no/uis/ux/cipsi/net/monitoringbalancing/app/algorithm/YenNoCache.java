37a6976f5232551c8b8204d6fd62f3659733da6c
Aryan TaheriMonfared <aryantaheri@gmail.com>
2015-04-07 14:54:20 +0200

    Make Topo and Algo serializable for further processing

--- src/main/java/no/uis/ux/cipsi/net/monitoringbalancing/app/algorithm/YenNoCache.java
+++ src/main/java/no/uis/ux/cipsi/net/monitoringbalancing/app/algorithm/YenNoCache.java
@@ -1,23 +1,27 @@
 package no.uis.ux.cipsi.net.monitoringbalancing.app.algorithm;
 
+import java.io.Serializable;
+
 import mulavito.algorithms.shortestpath.ksp.Yen;
 
 import org.apache.commons.collections15.Transformer;
 
 import edu.uci.ics.jung.graph.Graph;
 
-public class YenNoCache<V, E> extends Yen<V, E>{
+public class YenNoCache<V, E> extends Yen<V, E> implements Serializable{
+
+    private static final long serialVersionUID = 1L;
 
     public YenNoCache(Graph<V, E> graph,
             Transformer<E, Number> nev) {
         super(graph, nev);
         dijkstra.enableCaching(false);
-//        dijkstra.setMaxTargets(8);
+        //        dijkstra.setMaxTargets(8);
         dijkstra.setMaxDistance(8);
     }
-    
+
     public Graph<V,E> getGraph() {
-    	return graph;
-	}
-    
+        return graph;
+    }
+
 }
