7fb4953717f979b0a6d4319841b795c602802018
Aryan TaheriMonfared <aryantaheri@gmail.com>
2015-05-18 14:33:05 +0100

    - Add incrementalScoreCalculator for Monitoring - Update the solver.config to use it

--- src/main/java/no/uis/ux/cipsi/net/monitoringbalancing/solver/score/IncrementalScoreCalculator.java
+++ src/main/java/no/uis/ux/cipsi/net/monitoringbalancing/solver/score/IncrementalScoreCalculator.java
@@ -1,17 +1,63 @@
 package no.uis.ux.cipsi.net.monitoringbalancing.solver.score;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import no.uis.ux.cipsi.net.monitoringbalancing.app.TopologyManager;
+import no.uis.ux.cipsi.net.monitoringbalancing.domain.MonitoringBalance;
+import no.uis.ux.cipsi.net.monitoringbalancing.domain.MonitoringHost;
+import no.uis.ux.cipsi.net.monitoringbalancing.domain.Node;
+import no.uis.ux.cipsi.net.monitoringbalancing.domain.Switch;
 import no.uis.ux.cipsi.net.monitoringbalancing.domain.TrafficFlow;
+import no.uis.ux.cipsi.net.monitoringbalancing.domain.WeightedLink;
+import no.uis.ux.cipsi.net.monitoringbalancing.util.Configs;
 
-import org.optaplanner.core.api.score.Score;
+import org.optaplanner.core.api.score.buildin.hardsoftbigdecimal.HardSoftBigDecimalScore;
 import org.optaplanner.core.impl.score.director.incremental.AbstractIncrementalScoreCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class IncrementalScoreCalculator extends AbstractIncrementalScoreCalculator<TrafficFlow> {
+import edu.uci.ics.jung.graph.Graph;
 
+public class IncrementalScoreCalculator extends AbstractIncrementalScoreCalculator<MonitoringBalance> {
+
+    private static Logger log = LoggerFactory.getLogger(IncrementalScoreCalculator.class);
+    private static final float TUNNELLING_OVERHEAD = 0.2f;
+    private Map<WeightedLink, Double> linkUsageMap;
+    private Map<Switch, Double> switchFabricUsageMap;
+    private Map<Switch, Double> switchForwardingUsageMap;
+    private Map<MonitoringHost, Double> hostUsageMap;
+
+    private Map<Switch, Double> monitoringSwitchCostMap;
+    private Map<MonitoringHost, Double> monitoringHostCostMap;
+    private Map<WeightedLink, Double> monitoringLinkCostMap;
+
+    Graph<Node, WeightedLink> topology;
+    Configs configs;
+
+    double hardScore = 0;
+    double softScore = 0;
 
     @Override
-    public void resetWorkingSolution(TrafficFlow workingSolution) {
-        // TODO Auto-generated method stub
+    public void resetWorkingSolution(MonitoringBalance workingSolution) {
+        linkUsageMap = new HashMap<WeightedLink, Double>();
+        switchFabricUsageMap = new HashMap<Switch, Double>();
+        switchForwardingUsageMap = new HashMap<Switch, Double>();
+        hostUsageMap = new HashMap<MonitoringHost, Double>();
 
+        monitoringSwitchCostMap = new HashMap<Switch, Double>();
+        monitoringHostCostMap = new HashMap<MonitoringHost, Double>();
+        monitoringLinkCostMap = new HashMap<WeightedLink, Double>();
+
+        configs = workingSolution.getConfigs();
+        topology = workingSolution.getTopology();
+
+        List<TrafficFlow> trafficFlows = workingSolution.getTrafficFlows();
+        for (TrafficFlow trafficFlow : trafficFlows) {
+            insert(trafficFlow);
+        }
     }
 
     @Override
@@ -22,25 +68,26 @@ public class IncrementalScoreCalculator extends AbstractIncrementalScoreCalculat
 
     @Override
     public void afterEntityAdded(Object entity) {
-        // TODO Auto-generated method stub
-
+        insert((TrafficFlow) entity);
     }
 
     @Override
     public void beforeVariableChanged(Object entity, String variableName) {
-        // TODO Auto-generated method stub
-
+        // TODO: include variableName and limit the change domain
+        log.debug("beforeVariableChanged e={}, variableName={}", entity, variableName);
+        retract((TrafficFlow) entity);
     }
 
     @Override
     public void afterVariableChanged(Object entity, String variableName) {
-        // TODO Auto-generated method stub
-
+        // TODO: include variableName and limit the change domain
+        log.debug("afterVariableChanged e={}, variableName={}", entity, variableName);
+        insert((TrafficFlow) entity);
     }
 
     @Override
     public void beforeEntityRemoved(Object entity) {
-        // TODO Auto-generated method stub
+        retract((TrafficFlow) entity);
 
     }
 
@@ -51,19 +98,171 @@ public class IncrementalScoreCalculator extends AbstractIncrementalScoreCalculat
     }
 
     @Override
-    public Score calculateScore() {
-        // TODO Auto-generated method stub
-        return null;
+    public HardSoftBigDecimalScore calculateScore() {
+        return HardSoftBigDecimalScore.valueOf(BigDecimal.valueOf(hardScore), BigDecimal.valueOf(softScore));
     }
 
 
     private void insert(TrafficFlow trafficFlow) {
-        // TODO Auto-generated method stub
 
+        MonitoringHost monHost = trafficFlow.getMonitoringHost();
+        Switch monSwitch = trafficFlow.getMonitoringSwitch();
+
+        if (monHost == null || monSwitch == null) {
+            // TODO: is this right? or they need individual cases
+            return;
+        }
+
+        calculateTrafficFlowResourceUsage(trafficFlow, true);
+        calculateMonitoringResourceUsage(topology, configs, trafficFlow, true);
     }
 
     private void retract(TrafficFlow trafficFlow) {
-        // TODO Auto-generated method stub
+        MonitoringHost monHost = trafficFlow.getMonitoringHost();
+        Switch monSwitch = trafficFlow.getMonitoringSwitch();
+
+        if (monHost == null || monSwitch == null) {
+            // TODO: is this right? or they need individual cases
+            return;
+        }
+
+        calculateTrafficFlowResourceUsage(trafficFlow, false);
+        calculateMonitoringResourceUsage(topology, configs, trafficFlow, false);
+    }
+
+    private void calculateTrafficFlowResourceUsage(TrafficFlow trafficFlow, boolean added) {
+        List<WeightedLink> path = trafficFlow.getPath();
+        calculateTrafficFlowLinkUsage(trafficFlow, path, 0, added);
+        calculateTrafficFlowSwitchUsage(trafficFlow, path, added);
+        calculateTrafficFlowHostUsage(trafficFlow, path, added);
+    }
+
+    private void calculateTrafficFlowLinkUsage(TrafficFlow trafficFlow, List<WeightedLink> path, float overhead, boolean added) {
+        for (WeightedLink weightedLink : path) {
+            Double oldUsage = linkUsageMap.get(weightedLink);
+            if (oldUsage == null){
+                oldUsage = new Double(0);
+            }
+            Double newUsage = new Double(oldUsage);
+            if (added) {
+                newUsage += trafficFlow.getRate() + trafficFlow.getRate()*overhead;
+            } else {
+                newUsage -= trafficFlow.getRate() + trafficFlow.getRate()*overhead;
+            }
+            double oldAvailableBandwidth = weightedLink.getSpeed() - weightedLink.getUsage() - oldUsage;
+            double newAvailableBandwidth = weightedLink.getSpeed() - weightedLink.getUsage() - newUsage;
+
+            hardScore += Math.min(newAvailableBandwidth, 0) - Math.min(oldAvailableBandwidth, 0);
+            linkUsageMap.put(weightedLink, newUsage);
+        }
 
     }
+
+    private void calculateTrafficFlowHostUsage(TrafficFlow trafficFlow, List<WeightedLink> path, boolean added) {
+        // TODO Auto-generated method stub
+    }
+
+    private void calculateTrafficFlowSwitchUsage(TrafficFlow trafficFlow, List<WeightedLink> path, boolean added) {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * Resource Usage for monitoring traffic MonitoringSwitch -> MonitoringHost,
+     * which should account for the tunneling overhead as well.
+     * @param trafficFlow
+     */
+    private void calculateMonitoringResourceUsage(Graph<Node,WeightedLink> topology, Configs configs, TrafficFlow trafficFlow, boolean added) {
+        if (trafficFlow.getMonitoringHost() == null || trafficFlow.getMonitoringSwitch() == null) {
+            //            log.debug("calculateMonitoringResourceUsage trafficFlow={} missing monitoringSwitch={} or monitoringHost={}", trafficFlow, trafficFlow.getMonitoringSwitch(), trafficFlow.getMonitoringHost());
+            return;
+        }
+        //        log.debug("calculateMonitoringResourceUsage trafficFlow={} missing monitoringSwitch={} or monitoringHost={}", trafficFlow, trafficFlow.getMonitoringSwitch(), trafficFlow.getMonitoringHost());
+        // FIXME: This is wrong, inserting a flow with the path P1, and retracting the flow with the path P2
+        List<WeightedLink> path = TopologyManager.getRandomShortestPath(topology, configs, trafficFlow.getMonitoringSwitch(), trafficFlow.getMonitoringHost(), 4);
+        trafficFlow.setMonitoringSwitchHostPath(path);
+
+        calculateTrafficFlowLinkUsage(trafficFlow, path, TUNNELLING_OVERHEAD, added);
+        calculateTrafficFlowSwitchUsage(trafficFlow, path, added);
+        calculateTrafficFlowHostUsage(trafficFlow, path, added);
+
+        calculateMonitoringServiceSwitchCost(trafficFlow, added);
+        calculateMonitoringServiceHostCost(trafficFlow, added);
+        calculateMonitoringServiceLinkCost(trafficFlow, path, added);
+    }
+
+
+    private void calculateMonitoringServiceSwitchCost(TrafficFlow trafficFlow, boolean added) {
+        Switch monitoringSwitch = trafficFlow.getMonitoringSwitch();
+        Double oldUsage = monitoringSwitchCostMap.get(monitoringSwitch);
+        if (oldUsage == null){
+            oldUsage = new Double(0);
+        }
+        Double newUsage = new Double(oldUsage);
+
+        if (added){
+            if (newUsage == 0){
+                newUsage = new Double(monitoringSwitch.getInitCost());
+            }
+            newUsage += monitoringSwitch.getPerFlowReuseCost();
+
+        } else {
+            newUsage -= monitoringSwitch.getPerFlowReuseCost();
+            if (newUsage == monitoringSwitch.getInitCost()){
+                newUsage = new Double(0);
+            }
+        }
+        softScore -= (newUsage - oldUsage);
+        monitoringSwitchCostMap.put(monitoringSwitch, newUsage);
+    }
+
+    private void calculateMonitoringServiceHostCost(TrafficFlow trafficFlow, boolean added) {
+        MonitoringHost host = trafficFlow.getMonitoringHost();
+        Double oldUsage = monitoringHostCostMap.get(host);
+        if (oldUsage == null){
+            oldUsage = new Double(0);
+        }
+        Double newUsage = new Double(oldUsage);
+
+        if (added) {
+            if (oldUsage == 0) {
+                newUsage = new Double(host.getCost());
+            }
+            // Add a small value to keep track of the host reuse
+            newUsage += 1;
+
+        } else {
+            // Oops, how to understand if this is the last usage of this host???
+            // Use the +1 per reuse dummy cost to do so
+            newUsage -= 1;
+            if (newUsage == host.getCost()) {
+                newUsage = new Double(0);
+            }
+        }
+        softScore -= (newUsage - oldUsage);
+        monitoringHostCostMap.put(host, newUsage);
+    }
+
+
+    private void calculateMonitoringServiceLinkCost(TrafficFlow trafficFlow, List<WeightedLink> path, boolean added) {
+        for (WeightedLink weightedLink : path) {
+            Double oldCost = monitoringLinkCostMap.get(weightedLink);
+            if (oldCost == null){
+                oldCost = new Double(0);
+            }
+            Double newCost = new Double(oldCost);
+
+            if (added) {
+                newCost += weightedLink.getMonitorServiceCost(trafficFlow.getRate());
+            } else {
+                newCost -= weightedLink.getMonitorServiceCost(trafficFlow.getRate());
+            }
+
+            softScore -= (newCost - oldCost);
+            monitoringLinkCostMap.put(weightedLink, newCost);
+        }
+    }
+
+
+
+
 }
